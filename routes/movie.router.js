028ba7a5e4f918e0396e881f935d050e2e9b7da0
cdlavacudeg <cristianlav10@gmail.com>
2022-07-13 11:06:00 -0500

    [Add] Get query request and Get by id Movies

--- routes/movie.router.js
+++ routes/movie.router.js
@@ -6,8 +6,18 @@ const service = new MovieService()
 
 router.get('/', async (req, res, next) => {
   try {
-    const movies = await service.find()
+    const movies = await service.find(req.query)
     res.json(movies)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const movie = await service.findOne(id)
+    res.json(movie)
   } catch (error) {
     next(error)
   }
--- services/movie.service.js
+++ services/movie.service.js
@@ -1,11 +1,38 @@
 const { models } = require('../libs/sequelize')
 
 class MovieService {
-  async find() {
-    const movies = await models.Movie.findAll({
-      include: ['gender', 'characters'],
-    })
+  async find(query) {
+    const options = {
+      include: ['gender'],
+      attributes: ['id', 'image', 'title', 'createdAt'],
+      where: {},
+      order: [],
+    }
+    const { name: title } = query
+    if (title) {
+      options.where.title = title
+    }
+
+    const { genre: gender } = query
+    if (gender) {
+      options.where.genderId = gender
+    }
+
+    const { order } = query
+    if (order) {
+      options.order.push(['createdAt', order])
+    }
+    console.log(order)
+    const movies = await models.Movie.findAll(options)
     return movies
+  }
+
+  async findOne(id) {
+    const movie = await models.Movie.findByPk(id, {
+      include: ['gender', 'characters'],
+      attributes: { exclude: ['genderId'] },
+    })
+    return movie
   }
 }
 
