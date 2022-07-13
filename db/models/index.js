e0afff6481721034de31355b870b146660d1a5f3
cdlavacudeg <cristianlav10@gmail.com>
2022-07-13 09:41:12 -0500

    [Add] User DB model

--- db/models/index.js
+++ db/models/index.js
@@ -1,3 +1,4 @@
+const { User, UserSchema } = require('./user.model')
 const { Movie, MovieSchema } = require('./movie.model')
 const { Character, CharacterSchema } = require('./character.model')
 const { Gender, GenderSchema } = require('./gender.model')
@@ -6,6 +7,7 @@ const {
   MovieCharacterSchema,
 } = require('./movie-character.model')
 function setupModels(sequelize) {
+  User.init(UserSchema, User.config(sequelize))
   Movie.init(MovieSchema, Movie.config(sequelize))
   Character.init(CharacterSchema, Character.config(sequelize))
   MovieCharacter.init(MovieCharacterSchema, MovieCharacter.config(sequelize))
