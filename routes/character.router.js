1f4cf56988ec1b92105a76b86fbc43837cb6703b
cdlavacudeg <cristianlav10@gmail.com>
2022-07-13 21:28:19 -0500

    [Add] CRUD Character

--- routes/character.router.js
+++ routes/character.router.js
@@ -6,8 +6,50 @@ const service = new CharacterService()
 
 router.get('/', async (req, res, next) => {
   try {
-    const movies = await service.find()
+    const query = req.query
+    const movies = await service.find(query)
     res.json(movies)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const character = await service.findOne(id)
+    res.json(character)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const body = req.body
+    const newCharacter = await service.create(body)
+    res.status(201).json(newCharacter)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.patch('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const body = req.body
+    const characterUpdated = await service.update(id, body)
+    res.json(characterUpdated)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const characterDeleted = await service.delete(id)
+    res.json(characterDeleted)
   } catch (error) {
     next(error)
   }
--- services/character.service.js
+++ services/character.service.js
@@ -1,11 +1,58 @@
 const { models } = require('../libs/sequelize')
 
 class CharacterService {
-  async find() {
-    const characters = await models.Character.findAll({
+  async find(query) {
+    const options = {
+      attributes: ['id', 'image', 'name'],
+      include: [],
+      where: {},
+    }
+    const { name } = query
+    if (name) {
+      options.where.name = name
+    }
+
+    const { age } = query
+    if (age) {
+      options.attributes.push('age')
+      options.where.age = age
+    }
+
+    const { movies } = query
+    if (movies) {
+      options.include.push({
+        model: models.Movie,
+        as: 'movies',
+        where: {
+          id: movies,
+        },
+      })
+    }
+    const characters = await models.Character.findAll(options)
+    return characters
+  }
+  async findOne(id) {
+    const character = await models.Character.findByPk(id, {
       include: ['movies'],
     })
-    return characters
+    return character
+  }
+
+  async create(data) {
+    const newCharacter = await models.Character.create(data)
+    return newCharacter
+  }
+
+  async update(id, changes) {
+    const character = await models.Character.findByPk(id)
+    const updatedCharacter = await character.update(changes)
+    return updatedCharacter
+  }
+
+  async delete(id) {
+    const character = await models.Movie.findByPk(id)
+    await character.destroy()
+    return character
   }
 }
 
